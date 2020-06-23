d8e60d6fad075fc88d95a23568566b39e94f08d0
kennyWhiteCreations <kennywhitejr@hotmail.com>
2020-06-23 14:37:52 -0400

    first commit

---
+++ src/js/contact.js
@@ -0,0 +1,17 @@
+openNav = () => {
+  document
+    .getElementById("mobile-navbar")
+    .classList.replace("navbar-closed", "navbar-open");
+  document.getElementById("hamburger-link").style.width = "0";
+  document.getElementById("hamburger-link").style.opacity = "0";
+  document.getElementById("hamburger-link").style.height = "0";
+};
+
+closeNav = () => {
+  document
+    .getElementById("mobile-navbar")
+    .classList.replace("navbar-open", "navbar-closed");
+  document.getElementById("hamburger-link").style.width = "10vh";
+  document.getElementById("hamburger-link").style.opacity = "1";
+  document.getElementById("hamburger-link").style.height = "10vh";
+};
